38beb33c17d530c3ce4ee9c4f1bee4d47409d846
1Rendes <1admin@ukr.net>
2024-12-21 13:46:39 +0100

    loader change

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,14 +1,10 @@
 import css from "./Loader.module.css";
+import loader from "../../img/soitev_karavan.gif";
 
 const Loader = () => {
   return (
     <div className={css.centredWrapper}>
-      <img
-        width={200}
-        height={200}
-        src="/src/img/soitev_karavan.gif"
-        alt="Loading..."
-      />
+      <img width={200} height={200} src={loader} alt="Loading..." />
     </div>
   );
 };
