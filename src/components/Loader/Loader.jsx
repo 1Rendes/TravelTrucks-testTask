a5679aa64bac9342e7ec91200aed1a8d0bc41144
1Rendes <1admin@ukr.net>
2024-12-21 13:43:26 +0100

    loader change

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,17 +1,13 @@
-import { TailSpin } from "react-loader-spinner";
 import css from "./Loader.module.css";
 
 const Loader = () => {
   return (
     <div className={css.centredWrapper}>
-      <TailSpin
-        visible={true}
-        height="40"
-        width="40"
-        color="red"
-        ariaLabel="tail-spin-loading"
-        radius="1"
-        strokeWidth="7"
+      <img
+        width={200}
+        height={200}
+        src="/src/img/soitev_karavan.gif"
+        alt="Loading..."
       />
     </div>
   );
